2cd029764eb4e050ca9657f2a9902cb71f6a5498
Diogo Alves <diogobis23@gmail.com>
2024-08-30 15:06:41 -0300

    Home and layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { navigating } from "$app/stores";
+	import Nav from "./Nav.svelte";
+</script>
+
+{#if $navigating}
+	<p>Loading...</p>
+{/if}
+
+<div class="vh-100 d-flex flex-row align-items-stretch">
+	<Nav />
+
+	<main class="flex-grow-1">
+		<slot />
+	</main>
+</div>
