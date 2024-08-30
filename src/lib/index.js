97464cb07c9fb45b126a5a1e84153b781056c6c4
Diogo Alves <diogobis23@gmail.com>
2024-08-30 15:05:45 -0300

    Utils

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,46 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+export function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export class InfoBuilder {
+	constructor(elements = []) {
+		this.elements = elements;
+	}
+
+	addText(content) {
+		this.elements.push({
+			type: "text",
+			content,
+		});
+	}
+
+	addUnorderedList(content) {
+		this.elements.push({
+			type: "ulist",
+			content,
+		});
+	}
+
+	addOrderedList(content) {
+		this.elements.push({
+			type: "olist",
+			content,
+		});
+	}
+
+	addImage(content) {
+		this.elements.push({
+			type: "image",
+			content,
+		});
+	}
+
+	addBlock(content) {
+		this.elements.push({
+			type: "block",
+			content,
+		});
+	}
+}
