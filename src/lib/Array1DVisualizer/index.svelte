dee7cdc0e65b72397b03a33ebae122176fc048bf
Diogo Alves <diogobis23@gmail.com>
2024-08-31 03:28:03 -0300

    1D Array Visualizer

---
+++ src/lib/Array1DVisualizer/index.svelte
@@ -0,0 +1,16 @@
+<script>
+	// import Array2DVisualizer from "$lib/Array2DVisualizer/index.svelte";
+	import Array2DVisualizer from "../Array2DVisualizer/index.svelte";
+
+	export let title;
+	export let array;
+	export let selected;
+	export let accessed;
+</script>
+
+<Array2DVisualizer
+	title={title || "Algorithm 1D Visualizer"}
+	{selected}
+	{accessed}
+	array2D={[array]}
+></Array2DVisualizer>
