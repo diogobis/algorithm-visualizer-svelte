d0b961daae7c2ef7941e5715728eefba1763aa48
Diogo Alves <diogobis23@gmail.com>
2024-09-01 02:55:04 -0300

    Logger Limit Length

--- src/lib/Logger/index.svelte
+++ src/lib/Logger/index.svelte
@@ -18,10 +18,20 @@
 	class="h-100 p-2 d-flex flex-column align-items-start justify-content-start overflow-auto"
 	bind:this={logger}
 >
-	<span>{title || "Logger"}</span>
+	<span class="logger-text">{title || "Logger"}</span>
 	<!-- <span>{JSON.stringify(array2D)}</span> -->
 
 	{#each logs as log}
-		<span>{log}</span>
+		{#if log.length > 50}
+			<span>{log.substring(0, 50) + "..."}</span>
+		{:else}
+			<span>{log}</span>
+		{/if}
 	{/each}
 </div>
+
+<style>
+	.logger-text {
+		text-wrap: wrap;
+	}
+</style>
